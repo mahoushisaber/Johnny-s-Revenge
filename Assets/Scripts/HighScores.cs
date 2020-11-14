c7524dd896bf7284ea9bd75f2ebf68e94da1d1f2
AugeyT <71095950+AugeyT@users.noreply.github.com>
2020-11-14 04:24:03 -0800

    High Scores are implemented and functional
    - New High Score Scene is created with up to 5 high scores displayed
    - When player wins game with high score, they are asked to enter there name when returned to menu scene.
    - high scores are sorted and persistent between games.
    - High scores are shown from main scene with new button to show high scores.
    - From high score scene you can exit scene and reset the high scores with scroll buttons.

---
+++ Assets/Scripts/HighScores.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class HighScores : MonoBehaviour
+{
+    public Text HighScore1Text;
+    public Text HighScore1PlayerText;
+    public Text HighScore2Text;
+    public Text HighScore2PlayerText;
+    public Text HighScore3Text;
+    public Text HighScore3PlayerText;
+    public Text HighScore4Text;
+    public Text HighScore4PlayerText;
+    public Text HighScore5Text;
+    public Text HighScore5PlayerText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        HighScore1Text.text = HighScoreController.gCtrl.highScores[0].ToString();
+        HighScore1PlayerText.text = HighScoreController.gCtrl.players[0];
+        HighScore2Text.text = HighScoreController.gCtrl.highScores[1].ToString();
+        HighScore2PlayerText.text = HighScoreController.gCtrl.players[1];
+        HighScore3Text.text = HighScoreController.gCtrl.highScores[2].ToString();
+        HighScore3PlayerText.text = HighScoreController.gCtrl.players[2];
+        HighScore4Text.text = HighScoreController.gCtrl.highScores[3].ToString();
+        HighScore4PlayerText.text = HighScoreController.gCtrl.players[3];
+        HighScore5Text.text = HighScoreController.gCtrl.highScores[4].ToString();
+        HighScore5PlayerText.text = HighScoreController.gCtrl.players[4];
+    }
+
+    public void OnClick_Exit()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
