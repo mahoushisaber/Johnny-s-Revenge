74681b7b2b6d6e751eaf222ad848e006e4fa1c28
AugeyT <71095950+AugeyT@users.noreply.github.com>
2020-11-23 01:22:06 -0800

    Signifiers implemented
    - Added in game signifiers for nudging
      1. Shown when no card has been dropped in N configurable time
        - Shows arrows animated to drop zones available from players cards
        - Mana drop will not show if not enough mana to use
        - If active will disappear after a card has been played
        - Can only show a maximum of N configurable times during the game
      2. Mana "2x" arrow shows when mana has not been used N configurable times
        - Shows arrow animated to mana zone from players cards
        - Will not show if not enough mana to use for a card
        - If active will disappear after a card has been played
        - Independent of Signifier 1 but will not activate if signifier 1 is active.
        - Can only show a maximum of N configurable times during the game
    - Fixed some bugs
      1. Fixed so mana zone highlight will not activate on card pickup if there is not enough mana to use.
       2. Mana zone text was wrong color of black so would disappear when mana drops below 50%. Corrected with making text original color of white following theme of other health bars.
    - Brought in code from main to sync this branch with main

---
+++ Assets/Scripts/ArrowSignifiers.cs
@@ -0,0 +1,162 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ArrowSignifiers : MonoBehaviour
+{
+    public bool BattleZoneEnabled;
+    public bool ManaZoneEnabled;
+    public int MaxTimesZonesCanShow;
+    public float TimeUntilZonesShows;
+    public int MaxTimesMZCanShow;
+    public int TimesManaUnsedBeforeShow;
+    public Image BattleZoneSignifier;
+    public Image ManaZoneSignifier;
+
+    private int m_TimesZonesHaveShown;
+    private float m_ZonesToShowTmr;
+    private Animator m_BZAnimator;
+    private bool m_BZAnimPlaying;
+    private int m_TimesManaShown;
+    private Animator m_MZAnimator;
+    private bool m_MZAnimPlaying;
+    enum SigStateType { BEGIN_TURN, END_TURN, UNKNOWN };
+    private SigStateType m_SignifiersState = SigStateType.UNKNOWN;
+    private bool m_ManaAllowed;
+    private int m_ManaUnusedCount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // In case someone has forgoton to set the property
+        if (MaxTimesZonesCanShow == 0)
+        {
+            MaxTimesZonesCanShow = 3;
+        }
+
+        m_TimesZonesHaveShown = 0;
+        m_ZonesToShowTmr = 0.0f;
+        m_BZAnimPlaying = false;
+        m_TimesManaShown = 0;
+        m_MZAnimPlaying = false;
+        m_ManaAllowed = false;
+        m_ManaUnusedCount = 0;
+
+        m_BZAnimator = BattleZoneSignifier.GetComponent<Animator>();
+        m_MZAnimator = ManaZoneSignifier.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // checked if All are enabled
+        if (BattleZoneEnabled == true)
+        { 
+            // Process the mana signifier algorithim
+            ProcessZoneSignifiers();
+        }
+
+        // checked if mana enabled
+        if (ManaZoneEnabled == true)
+        {
+            // Process the mana signifier algorithim
+            ProcessManaArrowSignifier();
+        }
+    }
+
+    void ProcessZoneSignifiers()
+    {
+        // check if we have shown all the maximum times
+        if (m_TimesZonesHaveShown < MaxTimesZonesCanShow)
+        {
+            if (m_SignifiersState == SigStateType.BEGIN_TURN && m_BZAnimPlaying == false)
+            {
+                // check if the waiting time has expired for all
+                m_ZonesToShowTmr += Time.deltaTime;
+
+                if (m_ZonesToShowTmr > TimeUntilZonesShows)
+                {
+                    // All expired so turn on all signifier animation
+                    m_BZAnimator.enabled = true;
+                    m_BZAnimator.Play("BattleZone_Signifier");
+                    m_BZAnimPlaying = true;
+                    BattleZoneSignifier.gameObject.SetActive(true);
+                    if (m_ManaAllowed == true)
+                    {
+                        m_MZAnimator.enabled = true;
+                        m_MZAnimator.Play("ManaZone_Signifier");
+                        m_MZAnimPlaying = true;
+                        ManaZoneSignifier.gameObject.SetActive(true);
+                    }
+
+                    m_ZonesToShowTmr -= TimeUntilZonesShows;
+                }
+            }
+            // Its playing or now end of turn so watch for the termination
+            else if (   m_SignifiersState == SigStateType.END_TURN 
+                     && (m_BZAnimPlaying == true || m_MZAnimPlaying == true))
+            {
+                m_BZAnimator.StopPlayback();
+                m_BZAnimator.enabled = false;
+                m_BZAnimPlaying = false;
+                BattleZoneSignifier.gameObject.SetActive(false);
+                m_MZAnimator.StopPlayback();
+                m_MZAnimator.enabled = false;
+                m_MZAnimPlaying = false;
+                ManaZoneSignifier.gameObject.SetActive(false);
+                m_TimesZonesHaveShown++;
+            }
+        }
+    }
+
+    void ProcessManaArrowSignifier()
+    {
+        // check if we have shown all the maximum times
+        if (    m_ManaUnusedCount >= TimesManaUnsedBeforeShow 
+            &&  m_TimesManaShown < MaxTimesMZCanShow)
+        {
+            if (   m_SignifiersState == SigStateType.BEGIN_TURN 
+                && m_MZAnimPlaying == false && m_ManaAllowed == true)
+            {
+                // Mana expired so turn on Mana signifier animation
+                m_MZAnimator.enabled = true;
+                m_MZAnimator.Play("ManaZone_Signifier");
+                m_MZAnimPlaying = true;
+                ManaZoneSignifier.gameObject.SetActive(true);
+                m_ManaUnusedCount = 0;
+            }
+            // Its playing or now end of turn so watch for the termination
+            else if (   m_SignifiersState == SigStateType.END_TURN 
+                     && m_MZAnimPlaying == true)
+            {
+                m_MZAnimator.StopPlayback();
+                m_MZAnimator.enabled = false;
+                m_MZAnimPlaying = false;
+                ManaZoneSignifier.gameObject.SetActive(false);
+                m_TimesManaShown++;
+            }
+        }
+    }
+
+    public void StartTurn(bool ManaAvailable)
+    {
+        m_ManaAllowed = ManaAvailable;
+        m_SignifiersState = SigStateType.BEGIN_TURN;
+        m_ZonesToShowTmr = 0f;
+    }
+
+    public void EndTurn(bool ManaWasUsed)
+    {
+        if (m_ManaAllowed == true && ManaWasUsed == false)
+        {
+            m_ManaUnusedCount++;
+        }
+        else // (ManaWasUsed == true)
+        {
+            m_ManaUnusedCount = 0;
+        }
+        m_ManaAllowed = false;
+        m_SignifiersState = SigStateType.END_TURN;
+    }
+}
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -16,19 +16,20 @@ public class GameController : MonoBehaviour
     public Text DeckUI;
     public Image BattleZoneArea;
     public Image PlayerManaBarHighlightImage;
-    public Sprite BossSprite, BossSprite2, BossSprite3;
 
     private EnemyController Enemy;
     private PlayerController Player;
     private BackgroundController Background;
+    private ArrowSignifiers GameSignifiers;
 
     enum StateType { UNKNOWN, ENEMY_TURN, PLAYER_TURN, BATTLE, BATTLE_EVALUATE };
     private StateType gameState = StateType.ENEMY_TURN;
     private bool cardDropped = false;
     private float waitToBatTmr = 0.0f;
+    private bool manaUsedThisTurn = false;
 
-    enum PTurnState { WAIT_FOR_CARD_PLAY, DELAY_TO_END_TURN };
-    private PTurnState playerState = PTurnState.WAIT_FOR_CARD_PLAY;
+    enum PTurnState { START_TURN_INIT, WAIT_FOR_CARD_PLAY, DELAY_TO_END_TURN };
+    private PTurnState playerState = PTurnState.START_TURN_INIT;
 
     private PersistentGameSettings gameSettings;
     private Sprite BZAreaArtwork;
@@ -45,6 +46,7 @@ public class GameController : MonoBehaviour
         gameSettings = FindObjectOfType<PersistentGameSettings>();
         ScoreSystem = FindObjectOfType<ScoreSystem>();
         Background = FindObjectOfType<BackgroundController>();
+        GameSignifiers = FindObjectOfType<ArrowSignifiers>();
         BZAreaArtwork = BattleZoneArea.sprite;
         PMBarArtwork = PlayerManaBarHighlightImage.sprite;
 
@@ -62,7 +64,6 @@ public class GameController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //RenderBossSprites();
         switch (gameState)
         {
             case StateType.UNKNOWN:
@@ -91,26 +92,7 @@ public class GameController : MonoBehaviour
         StageText.text = string.Format("Stage: {0} of {1}", CurrentStage, TotalStages);
         DeckUI.text = Player.cardsInDeck.ToString();
     }
-    /*
-    void RenderBossSprites()
-    {
-        if (CurrentStage == 1)
-        {
-            gameObject.GetComponent<SpriteRenderer>().sprite = BossSprite;
 
-        }
-        if (CurrentStage == 2)
-        {
-            gameObject.GetComponent<SpriteRenderer>().sprite = BossSprite2;
-
-        }
-        if (CurrentStage == 3)
-        {
-            gameObject.GetComponent<SpriteRenderer>().sprite = BossSprite3;
-
-        }
-    }
-    */
     void Execute_EnemyTurn()
     {
         if (!Enemy.IsTurn)
@@ -128,6 +110,19 @@ public class GameController : MonoBehaviour
     {
         switch (playerState)
         {
+            case PTurnState.START_TURN_INIT:
+                // Do not move on to next turn until the score screen has disappeared!
+                if (ScoreSystem.ResultScreenShowing == false)
+                { 
+                    // Set up the signifiers so they will properly manage
+                    GameSignifiers.StartTurn(Player.Mana >= Player.ManaUseCost);
+                    manaUsedThisTurn = false;
+
+                    // Goto the new state
+                    playerState = PTurnState.WAIT_FOR_CARD_PLAY;
+                }
+                break;
+
             // Waiting for a card to be dropped in the dropzone
             case PTurnState.WAIT_FOR_CARD_PLAY:
                 if (cardDropped)
@@ -137,6 +132,10 @@ public class GameController : MonoBehaviour
                     // Set timer to wait perioid
                     waitToBatTmr = 0.0f;
 
+                    // Stop signifiers
+                    GameSignifiers.EndTurn(manaUsedThisTurn);
+
+                    // Goto the new state
                     playerState = PTurnState.DELAY_TO_END_TURN;
                 }
                 break;
@@ -151,7 +150,7 @@ public class GameController : MonoBehaviour
                     {
                         // Timer done reset player state machine move to battle state
                         waitToBatTmr = 0.0f;
-                        playerState = PTurnState.WAIT_FOR_CARD_PLAY;
+                        playerState = PTurnState.START_TURN_INIT;
                         gameState = StateType.BATTLE;
                         Player.EndTurn();
                     }
@@ -159,13 +158,13 @@ public class GameController : MonoBehaviour
                 else
                 {
                     // No Seconds to Battle set so immediately move to battle state
-                    playerState = PTurnState.WAIT_FOR_CARD_PLAY;
+                    playerState = PTurnState.START_TURN_INIT;
                     gameState = StateType.BATTLE;
                 }
                 break;
 
             default:
-                playerState = PTurnState.WAIT_FOR_CARD_PLAY;
+                playerState = PTurnState.START_TURN_INIT;
                 gameState = StateType.BATTLE;
                 Debug.Log("Game Controller - PlayerTurn State INVALID");
                 break;
@@ -238,11 +237,15 @@ public class GameController : MonoBehaviour
     {
         // Turn on highlight for available drop zones. Don't forget to save current to restore
         Sprite artwork = Resources.Load<Sprite>("Sprites/BZ-Highlight");
-
         BattleZoneArea.sprite = artwork;
-        Color PMBOrigColor = PlayerManaBarHighlightImage.color;
-        PMBOrigColor.a = 255f;
-        PlayerManaBarHighlightImage.color = PMBOrigColor;
+
+        // Don't turn on mana if there is not enough to use.
+        if (Player.Mana >= Player.ManaUseCost)
+        {
+            Color PMBOrigColor = PlayerManaBarHighlightImage.color;
+            PMBOrigColor.a = 255f;
+            PlayerManaBarHighlightImage.color = PMBOrigColor;
+        }
     }
 
     public void OnEndDrag(PointerEventData eventData)
@@ -271,6 +274,11 @@ public class GameController : MonoBehaviour
                     {
                         Debug.Log("Not enough mana to enhance");
                         canDrop = false;
+                    }
+                    else
+                    {
+                        // Are using mana so record use for signifier tracking
+                        manaUsedThisTurn = true;
                     }
                     break;
 
